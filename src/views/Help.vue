a26b126f0b63f52cf55806560a55690819c1d627
Roxana <ms.roxanarahimi@gmail.com>
2023-09-11 23:05:04 +0330

    first commit

---
+++ src/views/Help.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div id="details" class="basic-1">
+      <div class="container">
+        <div class="row px-3 ">
+          <div class="col-7 col-md-5 col-lg-6 col-xl-7">
+            <div class="image-container mb-4">
+              <img
+                class="img-fluid"
+                src="images/details-1.svg"
+                alt="طراحی سایت"
+              />
+            </div>
+            <!-- end of image-container -->
+          </div>
+          <!-- end of col -->
+          <div class="col-md-7 col-lg-6 col-xl-5">
+            <div class="text-container ps-md-3">
+                <div class="d-flex align-items-start mb-3">
+                <i class="fas fa-check mx-2 my-1 text-danger"></i>
+                  <div class="flex-1">
+                    <h5>مشاوره رایگان</h5>
+                    <small class="text-muted mb-4">مشاوره آنلاین، تلفنی و حضوری</small>
+                  </div>
+                </div>
+                <div class="d-flex align-items-start mb-3">
+                <i class="fas fa-check m-1 text-danger"></i>
+                  <div class="flex-1">
+                    <h5>طراحی نقشه سایت</h5>
+                    <small class="text-muted mb-4">
+                      تعیین تعداد و عنوان صفحات
+                      </small>
+                  </div>
+                </div>
+                <div class="d-flex align-items-start mb-3">
+                <i class="fas fa-check m-1 text-danger"></i>
+                  <div class="flex-1">
+                    <h5>امکانات و ویژگی ها</h5>
+                    <small class="text-muted mb-4">
+                      بررسی تمامی ویژگی ها، بعلاه گرافیک، ترکیب رنگ ...
+                      </small>
+                  </div>
+                </div>
+                <div class="d-flex align-items-start mb-3">
+                <i class="fas fa-check m-1 text-danger"></i>
+                  <div class="flex-1">
+                    <h5>قرارداد</h5>
+                    <small class="text-muted mb-4">
+                      عقد قرارداد به صورت غیر حضوری و یا حضوری با هماهنگی قبلی
+                      </small>
+                  </div>
+                </div>
+                <div class="d-flex align-items-start mb-3">
+                <i class="fas fa-check m-1 text-danger"></i>
+                  <div class="flex-1">
+                    <h5>پشتیبانی</h5>
+                    <small class="text-muted mb-4">
+                  یک سال پشتیبانی رایگان هدیه تیم مشاوران وب
+                    </small>
+                  </div>
+                </div>
+              <!-- <h2><span>Perfect solution</span><br> for your small business</h2> -->
+              <!-- <p>Maecenas fringilla quam posuere, pellentesque est nec, gravida turpis. Integer vitae mollis felis. Integer id quam id tellus hendrerit laciniad binfer</p>
+                        <p>Sed id dui rutrum, dictum urna eu, accumsan turpis. Fusce id auctor velit, sed viverra dui rem dina</p>
+                        <a class="btn-solid-reg" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Modal</a> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/Portfolio.vue
@@ -0,0 +1,165 @@
+<template>
+  <div>
+    <div class = "container">
+      <div class = "cards-1 row justify-content-start">
+        <div class = "col-6 col-md-4 col-lg-3 col-xxl-3 mb-3">
+          <a href = "https://mycopa.ir" target = "_blank" class = "card">
+            <img
+                class = "card-img-top"
+                src = "images/copa.jpg"
+                alt = "طراحی سایت"
+            />
+            <div class = "card-body ">
+              <h5 class = "card-title ms-auto me-3 text-center my-md-2">
+                کوپا
+              </h5>
+            </div>
+          </a>
+        </div>
+        <div class = "col-6 col-md-4 col-lg-3 col-xxl-3 mb-3">
+          <a href = "https://elitefood.ir" target = "_blank" class = "card">
+            <img
+                class = "card-img-top"
+                src = "images/elite.jpg"
+                alt = "طراحی سایت"
+            />
+            <div class = "card-body ">
+              <h5 class = "card-title ms-auto me-3 text-center my-md-2">
+                نودالیت
+              </h5>
+            </div>
+          </a>
+        </div>
+        <div class = "col-6 col-md-4 col-lg-3 col-xxl-3 mb-3">
+          <a href = "https://shop2.webagent.ir" target = "_blank" class = "card">
+            <img
+                class = "card-img-top"
+                src = "images/shop2.jpg"
+                alt = "طراحی سایت"
+            />
+            <div class = "card-body ">
+              <h5 class = "card-title ms-auto me-3 text-center my-md-2">
+                فروشگاه
+              </h5>
+            </div>
+          </a>
+        </div>
+        <div class = "col-6 col-md-4 col-lg-3 col-xxl-3 mb-3">
+          <a href = "https://panel.shop2.webagent.ir/panel/login" target = "_blank" class = "card">
+            <img
+                class = "card-img-top"
+                src = "images/shop.panel.jpg"
+                alt = "طراحی سایت"
+            />
+            <div class = "card-body ">
+              <h5 class = "card-title ms-auto me-3 text-center my-md-2">
+                پنل فروشگاه
+              </h5>
+            </div>
+          </a>
+        </div>
+        <div class = "col-6 col-md-4 col-lg-3 col-xxl-3 mb-3">
+          <a href = "https://res.webagent.ir" target = "_blank" class = "card">
+            <img
+                class = "card-img-top"
+                src = "images/delish.jpg"
+                alt = "طراحی سایت"
+            />
+            <div class = "card-body ">
+              <h5 class = "card-title ms-auto me-3 text-center my-md-2">
+                رستوران
+              </h5>
+            </div>
+          </a>
+        </div>
+        <div class = "col-6 col-md-4 col-lg-3 col-xxl-3 mb-3">
+          <a href = "https://school.webagent.ir" target = "_blank" class = "card">
+            <img
+                class = "card-img-top"
+                src = "images/ielts.jpg"
+                alt = "طراحی سایت"
+            />
+            <div class = "card-body ">
+              <h5 class = "card-title ms-auto me-3 text-center my-md-2">
+                آموزشگاه </h5>
+            </div>
+          </a>
+        </div>
+        <div class = "col-6 col-md-4 col-lg-3 col-xxl-3 mb-3">
+          <a href = "https://highjob.webagent.ir" target = "_blank" class = "card">
+            <img
+                class = "card-img-top"
+                src = "/images/highjob.png"
+                alt = "طراحی سایت"
+            />
+            <div class = "card-body ">
+              <h5 class = "card-title ms-auto me-3 text-center my-md-2">
+                کار یابی </h5>
+            </div>
+          </a>
+        </div>
+        <div class = "col-6 col-md-4 col-lg-3 col-xxl-3 mb-3">
+          <a href = "https://old.webagent.ir" target = "_blank" class = "card">
+            <img
+                class = "card-img-top"
+                src = "images/old.jpg"
+                alt = "طراحی سایت"/>
+            <div class = "card-body">
+              <h5 class = "card-title ms-auto me-3 text-center my-md-2">
+                web agent
+              </h5>
+            </div>
+          </a>
+        </div>
+        <div class = "col-6 col-md-4 col-lg-3 col-xxl-3 mb-3">
+          <a href = "https://shop.webagent.ir" target = "_blank" class = "card">
+            <img
+                    class = "card-img-top"
+                    src = "images/shop.jpg"
+                    alt = "طراحی سایت"
+            />
+            <div class = "card-body ">
+              <h5 class = "card-title ms-auto me-3 text-center my-md-2">
+                فروشگاه
+              </h5>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+
+    };
+</script>
+
+<style scoped>
+  a,
+  a:hover {
+    text-decoration: none;
+  }
+
+  .cards-1 .card {
+    border-radius: 10px !important;
+  }
+
+  .card-img-top {
+    border-top-left-radius: 10px !important;
+    border-top-right-radius: 10px !important;
+  }
+
+  .card-body img {
+    border-radius: 10px !important;
+    height: 80px;
+  }
+
+  @media (max-width: 768px) {
+    .card-title {
+      line-height: 80px;
+      margin-bottom: 0 !important;
+    }
+  }
+</style>
